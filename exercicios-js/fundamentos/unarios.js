e3d8693e008ef108a7b3c22ee9814a2a79e6e85e
jeversonneves <93106179+jeversonneves@users.noreply.github.com>
2022-06-24 11:51:10 -0300

    Operadores Unários

---
+++ exercicios-js/fundamentos/unarios.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // pós fixado.
+console.log('Pós fixada:', num1) // num1 = num1 + 1
+
+--num1 // pré fixada.
+console.log('Pré fixada:',num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
