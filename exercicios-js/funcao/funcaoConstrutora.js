f5e1f962cec10072d3ed137ce0a5f80304498ad6
jeversonneves <93106179+jeversonneves@users.noreply.github.com>
2022-06-29 15:58:24 -0300

    Função Construtura

---
+++ exercicios-js/funcao/funcaoConstrutora.js
@@ -0,0 +1,31 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const Ferrari = new Carro(350, 20)
+Ferrari.acelerar()
+Ferrari.acelerar()
+Ferrari.acelerar()
+console.log(Ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof Ferrari)
