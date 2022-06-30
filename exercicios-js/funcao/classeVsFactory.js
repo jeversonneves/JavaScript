dffec6a7892204eb7d7304a24833bbb3d261e85d
jeversonneves <93106179+jeversonneves@users.noreply.github.com>
2022-06-30 15:42:43 -0300

    Classe vs Factory

---
+++ exercicios-js/funcao/classeVsFactory.js
@@ -0,0 +1,24 @@
+// Classe
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    // Metodo
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+// Factory
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`) 
+    }
+}
+
+const p2 = criarPessoa('Dennis')
+p2.falar()
