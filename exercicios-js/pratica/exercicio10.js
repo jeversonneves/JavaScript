2df45e99625a499cf1b24c0854f0b3076632c406
jeversonneves <93106179+jeversonneves@users.noreply.github.com>
2022-07-07 08:55:55 -0300

    exercício divisão

---
+++ exercicios-js/pratica/exercicio10.js
@@ -0,0 +1,12 @@
+function divisivelPorTres(numero) {
+
+    if ((numero % 3) === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(divisivelPorTres(18))
+console.log(divisivelPorTres(2))
+console.log(divisivelPorTres(150))
